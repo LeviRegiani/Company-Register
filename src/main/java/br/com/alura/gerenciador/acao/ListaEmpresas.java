ab79584765469d10fbb268b8aea624a49d229e1b
LeviRegiani <levi.regiani@usp.br>
2022-03-18 17:10:20 -0300

    login page working

--- src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -11,13 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-public class ListaEmpresas implements Acao{
-	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class ListaEmpresas implements Acao {
+	public String executa(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 		System.out.println("Listando Empresa");
 		request.setAttribute("empresas", lista);
-		
+
 		return "forward:listaEmpresas.jsp";
 	}
 }
--- src/main/java/br/com/alura/gerenciador/modelo/Banco.java
+++ src/main/java/br/com/alura/gerenciador/modelo/Banco.java
@@ -19,7 +19,7 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		lista.add(empresa);
 		lista.add(empresa2);
-		
+
 		Usuario u1 = new Usuario();
 		u1.setLogin("Nico");
 		u1.setSenha("12345");
@@ -55,6 +55,16 @@ public class Banco {
 		for (Empresa empresa : lista) {
 			if (empresa.getId() == id) {
 				return empresa;
+			}
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		// TODO Auto-generated method stub
+		for (Usuario usuario : listaUsuarios) {
+			if (usuario.ehIgual(login, senha)) {
+				return usuario;
 			}
 		}
 		return null;
